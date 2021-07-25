52616dab326747b784d349d176a73dfeada5471a
88016 <sujoy2838@gmail.com>
2021-07-25 18:46:08 +0600

    Adding products to our homepage

--- lib/ui/bottom_nav_pages/home.dart
+++ lib/ui/bottom_nav_pages/home.dart
@@ -13,31 +13,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
- List <String> _carouselImages = [];
- var _dotPosition = 0;
+  List<String> _carouselImages = [];
+  var _dotPosition = 0;
+  List _products = [];
+  var _firestoreInstance = FirebaseFirestore.instance;
   TextEditingController _searchController = TextEditingController();
-  
-  fetchCarouselImages()async{
-    var _firestoreInstance = FirebaseFirestore.instance;
-    QuerySnapshot qn = await _firestoreInstance.collection("carousel-slider").get();
+
+  fetchCarouselImages() async {
+
+    QuerySnapshot qn =
+        await _firestoreInstance.collection("carousel-slider").get();
     setState(() {
-      for(int i =0; i<qn.docs.length; i++){
+      for (int i = 0; i < qn.docs.length; i++) {
         _carouselImages.add(
           qn.docs[i]["img-path"],
-
         );
         print(qn.docs[i]["img-path"]);
       }
     });
 
     return qn.docs;
-
   }
+
+  fetchProducts() async {
+
+    QuerySnapshot qn =
+    await _firestoreInstance.collection("products").get();
+    setState(() {
+      for (int i = 0; i < qn.docs.length; i++) {
+        _products.add(
+         {
+           "product-name": qn.docs[i]["product-name"],
+           "product-description": qn.docs[i]["product-description"],
+           "product-price": qn.docs[i]["product-price"],
+           "product-img": qn.docs[i]["product-img"],
+         }
+
+        );
+
+      }
+    });
+
+    return qn.docs;
+  }
+
+
 
   @override
   void initState() {
     fetchCarouselImages();
+    fetchProducts();
     super.initState();
   }
 
@@ -49,71 +74,81 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             Padding(
-              padding:  EdgeInsets.only(left: 20.w,right: 20.w),
+              padding: EdgeInsets.only(left: 20.w, right: 20.w),
               child: Row(
                 children: [
-                  Expanded(child: SizedBox(
-                    height: 60.h,
-                    child: TextFormField(
-                      controller: _searchController,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(0)),
-                          borderSide: BorderSide(
-                            color: Colors.blue
-                          )
+                  Expanded(
+                    child: SizedBox(
+                      height: 60.h,
+                      child: TextFormField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(0)),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(0)),
+                              borderSide: BorderSide(color: Colors.grey)),
+                          hintText: "Search products here",
+                          hintStyle: TextStyle(fontSize: 15.sp),
                         ),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(0)),
-                            borderSide: BorderSide(
-                                color: Colors.grey
-                            )
-                        ),
-                        hintText: "Search products here",
-                        hintStyle: TextStyle(fontSize: 15.sp),
                       ),
                     ),
-                  ),),
+                  ),
                   GestureDetector(
                     child: Container(
                       height: 60.h,
                       width: 60.h,
                       color: AppColors.deep_orange,
                       child: Center(
-                        child: Icon(Icons.search,color: Colors.white,),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                    onTap: (){},
+                    onTap: () {},
                   )
                 ],
               ),
             ),
-            SizedBox(height: 10.h,),
-            AspectRatio(aspectRatio: 3.5,
-              child: CarouselSlider(items: _carouselImages.map((item) => Padding(
-                padding: const EdgeInsets.only(left: 3,right: 3),
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(image: NetworkImage(item),fit: BoxFit.fitWidth)
-                  ),
-                ),
-              )).toList(), options: CarouselOptions(
-                autoPlay: false,
-                enlargeCenterPage: true,
-                viewportFraction: 0.8,
-                enlargeStrategy: CenterPageEnlargeStrategy.height,
-                onPageChanged: (val, carouselPageChangedReason){
-                setState(() {
-                  _dotPosition=val;
-                });
-                }
-              )),
+            SizedBox(
+              height: 10.h,
             ),
-            SizedBox(height: 10.h,),
-
+            AspectRatio(
+              aspectRatio: 3.5,
+              child: CarouselSlider(
+                  items: _carouselImages
+                      .map((item) => Padding(
+                            padding: const EdgeInsets.only(left: 3, right: 3),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(item),
+                                      fit: BoxFit.fitWidth)),
+                            ),
+                          ))
+                      .toList(),
+                  options: CarouselOptions(
+                      autoPlay: false,
+                      enlargeCenterPage: true,
+                      viewportFraction: 0.8,
+                      enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      onPageChanged: (val, carouselPageChangedReason) {
+                        setState(() {
+                          _dotPosition = val;
+                        });
+                      })),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
             DotsIndicator(
-              dotsCount: _carouselImages.length==0?1:_carouselImages.length,
+              dotsCount:
+                  _carouselImages.length == 0 ? 1 : _carouselImages.length,
               position: _dotPosition.toDouble(),
               decorator: DotsDecorator(
                 activeColor: AppColors.deep_orange,
@@ -121,11 +156,31 @@ class _HomeState extends State<Home> {
                 spacing: EdgeInsets.all(2),
                 activeSize: Size(8, 8),
                 size: Size(6, 6),
-
               ),
+            ),
+            SizedBox(
+              height: 15.h,
+            ),
+            Expanded(
+              child: GridView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 1),
+                  itemBuilder: (_,index){
+                return Card(
+                  elevation: 3,
+                  child: Column(
+                    children: [
+                      AspectRatio(aspectRatio: 2,child: Container(color: Colors.yellow,child: Image.network(_products[index]["product-img"][0],))),
+                      Text("${_products[index]["product-name"]}"),
+                      Text("${_products[index]["product-price"].toString()}"),
+                    ],
+                  ),
+                );
+              }),
             )
 
-            
+
 
           ],
         ),
